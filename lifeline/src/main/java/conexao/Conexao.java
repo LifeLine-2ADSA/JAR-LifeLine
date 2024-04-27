28d7d37dc9543ea83021c8a0c18a5135690b504e
GabrielGuedesSPTECH <gabriel.gmacedo@sptech.school>
2024-04-27 13:01:03 -0300

    "Adicionando usuário padrão lifeline"

--- lifeline/src/main/java/conexao/Conexao.java
+++ lifeline/src/main/java/conexao/Conexao.java
@@ -2,20 +2,22 @@ package conexao;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
+
 public class Conexao {
 
-    private JdbcTemplate conexao;
+  private JdbcTemplate conexao;
 
-    public Conexao(){
-        BasicDataSource data = new BasicDataSource();
-        data.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        data.setUrl("jdbc:mysql://localhost:3306/lifeline");
-        data.setUsername("root");
-        data.setPassword("215912");
+  public Conexao() {
+    BasicDataSource data = new BasicDataSource();
+    data.setDriverClassName("com.mysql.cj.jdbc.Driver");
+    data.setUrl("jdbc:mysql://localhost:3306/lifeline");
+    data.setUsername("lifeline_user");
+    data.setPassword("urubu100");
 
-        conexao = new JdbcTemplate(data);
-    }
-    public JdbcTemplate getConexao(){
-        return conexao;
-    }
+    conexao = new JdbcTemplate(data);
+  }
+
+  public JdbcTemplate getConexao() {
+    return conexao;
+  }
 }
